8c44d6ddc4ac648a7e2a6b1dc5a2e31a1ebab157
Jenny Lin <misslinyang91@gmail.com>
2021-09-01 18:21:50 +1000

    made the image generator functional

--- app.js
+++ app.js
@@ -2,3 +2,41 @@
 Public API Repo
 https://github.com/public-apis/public-apis
 */
+let cat = document.querySelector(".cat");
+let fox = document.querySelector(".fox");
+let dog = document.querySelector(".dog");
+
+const catBtn = document.querySelector(".get-cat");
+const foxBtn = document.querySelector(".get-fox");
+const dogBtn = document.querySelector(".get-dog");
+
+catBtn.addEventListener("click", getRandomCat);
+foxBtn.addEventListener("click", getRandomFox);
+dogBtn.addEventListener("click", getRandomDog);
+
+//------------------get random cat
+function getRandomCat() {
+  fetch("https://aws.random.cat/meow")
+    .then((response) => response.json())
+    .then((responseData) => {
+      cat.innerHTML = `<img src="${responseData.file}" />`;
+    });
+}
+
+// ----------------------------------------------------Get Random Fox Images
+function getRandomFox() {
+  fetch("https://randomfox.ca/floof/")
+    .then((response) => response.json())
+    .then((responseData) => {
+      fox.innerHTML = `<img src="${responseData.image}" />`;
+    });
+}
+
+// ----------------------------------------------------Get Random Dog Images
+function getRandomDog() {
+  fetch("https://random.dog/woof.json")
+    .then((response) => response.json())
+    .then((responseData) => {
+      dog.innerHTML = `<img src="${responseData.url}" />`;
+    });
+}
